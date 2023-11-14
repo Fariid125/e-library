0ec615c0479362ee892e37ca696fd446cc7032b1
fariid125 <faridayomide10@gmail.com>
2023-11-14 14:37:07 +0100

    another commit

---
+++ Repository/BookCategoryRepository.cs
@@ -0,0 +1,45 @@
+﻿using E_Library.Controllers;
+using E_Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E_Library.Repository
+{
+    public class BookCategoryRepository : IBookCategoryRepository
+    {
+        private readonly ApplicationContext _context;
+        public BookCategoryRepository(ApplicationContext applicationContext)
+        {
+            _context = applicationContext;
+        }
+        public void CreateCategory(BookCategory category)
+        {
+            var categories = new BookCategory
+            {
+                Category = category.Category, 
+            };
+            _context.BookCategories.Add(categories);
+            _context.SaveChanges();
+        }
+
+        public void DeleteCategory(int id)
+        {
+            var category = _context.BookCategories.Find(id);
+            _context.BookCategories.Remove(category);
+            _context.SaveChanges();
+            _context.SaveChanges();
+        }
+
+        public List<BookCategory> GetAllCategory()
+        {
+            var categories = _context.BookCategories.ToList();
+            return categories;
+        }
+
+        public BookCategory GetCategory(int Id)
+        {
+            var category = _context.BookCategories.Find(Id);
+            return category;
+        }
+    }
+}
